ba5783370f52642bd31c9da2152b99faab51fef4
Kevin Ahmadi <kevinahmadi@gmail.com>
2022-04-16 08:59:32 -0700

    Initial.

---
+++ head/css.php
@@ -0,0 +1,5 @@
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@700&display=swap" rel="stylesheet"> 
+<link href="/css/brands.css" rel="stylesheet">
+<link href="/css/main.css" rel="stylesheet">
